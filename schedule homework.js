63feddff4a70f222af2ba2704ec67fcf631453f2
Mariem Hassan Hussien Ibrahiem <53480373+MariemHHussien@users.noreply.github.com>
2023-12-13 15:54:51 +0200

    Add files via upload

---
+++ schedule homework.js
@@ -0,0 +1,411 @@
+function clickme()
+{
+    var di=document.getElementById("divo")
+    
+    var input=document.createElement("input");
+    input.type="text";
+    // input.className="what"
+    di.appendChild(input);    
+}
+function clickme1()
+{
+    var di=document.getElementById("divo1")
+    
+    var input=document.createElement("input");
+    input.type="text";
+    // input.className="what"
+    di.appendChild(input);    
+
+}
+function sec()
+{
+var id=document.getElementById("section")
+var input=document.createElement("input");
+var date=document.getElementById("sec1");
+var date2=document.createElement("input");
+input.type="text";
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+
+}
+function lab()
+{
+var id=document.getElementById("lab");
+var input=document.createElement("input");
+var date=document.getElementById("lab1");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function add()
+{
+
+var id=document.getElementById("add");
+var input=document.createElement("input");
+var date=document.getElementById("add1");
+var date2=document.createElement("input");
+input.type="text";
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+input.style.placeholder="subject";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+
+}
+function add2()
+{
+var id=document.getElementById("add2");
+var input=document.createElement("input");
+var date=document.getElementById("add3");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function add3()
+{
+var id=document.getElementById("add4");
+var input=document.createElement("input");
+var date=document.getElementById("add5");
+var date2=document.createElement("input");
+input.type="text";
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function add4()
+{
+var id=document.getElementById("add6");
+var input=document.createElement("input");
+var date=document.getElementById("add7");
+var date2=document.createElement("input");
+input.type="text";
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function add5()
+{
+var id=document.getElementById("add8");
+var input=document.createElement("input");
+var date=document.getElementById("add9");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function today()
+{
+var id=document.getElementById("today");
+var input=document.createElement("input");
+var date=document.getElementById("today1");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function today1()
+{
+var id=document.getElementById("today2");
+var input=document.createElement("input");
+var date=document.getElementById("today3");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+
+function today2()
+{
+var id=document.getElementById("today4");
+
+var input=document.createElement("input");
+var date=document.getElementById("today5");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function day()
+{
+var id=document.getElementById("day1");
+
+var input=document.createElement("input");
+var date=document.getElementById("day2");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function day1()
+{
+var id=document.getElementById("day3");
+
+var input=document.createElement("input");
+var date=document.getElementById("day4");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function day2()
+{
+var id=document.getElementById("day5");
+
+var input=document.createElement("input");
+var date=document.getElementById("day6");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function day3()
+{
+var id=document.getElementById("day7");
+
+var input=document.createElement("input");
+var date=document.getElementById("day8");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function day4()
+{
+var id=document.getElementById("day9");
+
+var input=document.createElement("input");
+var date=document.getElementById("day10");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
+function day5()
+{
+var id=document.getElementById("day11");
+
+var input=document.createElement("input");
+var date=document.getElementById("day12");
+var date2=document.createElement("input");
+input.type="text";
+
+date2.type="date";
+id.appendChild(input);
+date.appendChild(date2);
+input.placeholder="subject";
+input.style.color="#1a0f68";
+input.style.fontSize="17px";
+input.style.backgroundColor="#f0d8d8";
+input.style.borderRadius="12px";
+input.style.borderColor="white";
+input.style.borderTopColor="white";
+input.style.textAlign="center";
+date2.style.backgroundColor="white";
+date2.style.borderRadius="12px";
+date2.style.borderColor="#e2a9a9";
+date2.style.textAlign="center";
+}
